803a8dd59ccd8092b5a923768753c3385d3005fb
Christof Petig <christof.petig@arcor.de>
2026-04-19 22:39:57 +0200

    show annotations (contain the fields)

--- extract/src/main.rs
+++ extract/src/main.rs
@@ -1,18 +1,23 @@
 use lopdf::Document;
 
 fn main() -> anyhow::Result<()> {
-    let bytes = std::fs::read("../amonedthorr1.pdf").unwrap();
+    let bytes = std::fs::read("form0.pdf").unwrap();
     let doc = Document::load_mem(&bytes)?;
     let pages = doc.get_pages();
     println!("{} Pages", pages.len());
     for (npage, oid) in doc.page_iter().enumerate() {
         println!("#Page {npage}");
         if let Ok(cont) = doc.get_page_annotations(oid) {
-            for ann in cont.iter() {
-                for (_, obj) in ann.iter() {
-                    dbg!(obj);
-                }
-            }
+            dbg!(cont);
+            // for ann in cont.iter() {
+            //     for (nm, obj) in ann.iter() {
+            //         if let Ok(s) = str::from_utf8(nm) {
+            //             dbg!((s, obj));
+            //         } else {
+            //             dbg!((nm, obj));
+            //         }
+            //     }
+            // }
         }
     }
     //let out = pdf_extract::extract_text_from_mem(&bytes).unwrap();
